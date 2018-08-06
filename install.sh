8ce4addcbbf63efc4f548bbec08810d751952a98
吴良伟 <wuliangwei@dianwoda.com>
2018-08-06 10:11:50 +0800

    fix(vim): 解决Vim: Warning: Input is not from a terminal

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ curl -o $HOME/.vimrc https://raw.githubusercontent.com/prerabale/vim-setting/plu
 
 echo "[vim-setting]: vim plugin manager plug installing..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || { echo "install plug        failure";exit -1; }
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || { echo "install plug failure";exit -1; }
 
 
-vim -c 'PlugInstall' -c 'qa!' || { echo "install vim plugin failure"; exit -1; }
+vim -c 'PlugInstall' -c 'qa!' < /dev/tty || { echo "install vim plugin failure"; exit -1; }
 echo "[vim-setting]: vim plugin installed successfully!"
 
-# cp -r ~/.vim/bundle/molokai/colors ~/.vim/ || { echo "vim theme [molokai] uninstall. please        install molokai first."; exit -1; }
+# cp -r ~/.vim/bundle/molokai/colors ~/.vim/ || { echo "vim theme [molokai] uninstall. please install molokai first."; exit -1; }
 
 echo "[vim-setting]: install successfully. have fun!"
